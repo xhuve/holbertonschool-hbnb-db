277b1701779cf7aa0419ee56b4edd76c61bcf4b0
xhuve <xhuvelialbana@gmail.com>
2024-06-12 10:49:10 +0200

    Finish place_controller

--- models/Place.py
+++ models/Place.py
@@ -2,7 +2,7 @@ from models.base_model import BaseModel
 
 class Place(BaseModel):
 
-    def __init__(self, name="", description="", address="", longitude=0.0, latitude=0.0, price_per_night=0.0, number_of_rooms=0, bathrooms=0, max_guests=0, amenity_id=(), city_id="", user_id=""):
+    def __init__(self, name="", description="", address="", longitude=0.0, latitude=0.0, price_per_night=0.0, number_of_rooms=0, bathrooms=0, max_guests=0, amenity_id=(), city_id="", host_id=""):
         super().__init__()
         self.name = name
         self.description = description
@@ -15,4 +15,4 @@ class Place(BaseModel):
         self.max_guests = max_guests
         self.amenity_id = amenity_id
         self.city_id = city_id
-        self.host_id = user_id
+        self.host_id = host_id
