cd1373749e78a3f254c7aae72bda87a384eb7242
xhuve <xhuvelialbana@gmail.com>
2024-06-12 12:37:03 +0200

    Fix review model, Add route n logic to create a review

--- models/Review.py
+++ models/Review.py
@@ -2,8 +2,10 @@ from models.base_model import BaseModel
 
 class Review(BaseModel):
 
-    def __init__(self, feedback="", rating="", comment=""):
+    def __init__(self, feedback="", rating="", comment="", place_id=None, user_id=None):
         super().__init__()
         self.feedback = feedback
         self.rating = rating
         self.comment = comment
+        self.place_id = place_id
+        self.user_id = user_id
